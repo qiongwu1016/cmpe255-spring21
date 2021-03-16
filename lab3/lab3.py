f215c09a2878417871ce874a85b56bbd33d441af
qiongwu1016 <qiongwu1016@gmail.com>
2021-03-16 00:04:53 -0700

    lab 3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -11,29 +11,30 @@ mpl.rc('xtick', labelsize=12)
 mpl.rc('ytick', labelsize=12)
 
 # Where to save the figures
-PROJECT_ROOT_DIR = "."
-IMAGE_DIR = "FIXME"
+PROJECT_ROOT_DIR = "/Users/qiongwu/git/cmpe255-spring21/lab3"
+IMAGE_DIR = ""
 
 def save_fig(fig_id, tight_layout=True):
     path = os.path.join(PROJECT_ROOT_DIR, "images", IMAGE_DIR, fig_id + ".png")
-    print("Saving figure", fig_id)
+    print("Saving figure (index: ", fig_id, ')')
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format='png', dpi=300)
     
 
-def random_digit():
-    some_digit = X[36000]
+def random_digit(X, ind):
+
+    some_digit = X[ind]
     some_digit_image = some_digit.reshape(28, 28)
     plt.imshow(some_digit_image, cmap = mpl.cm.binary,
             interpolation="nearest")
     plt.axis("off")
 
-    save_fig("some_digit_plot")
+    save_fig("%d"%ind)
     plt.show()
 
    
-def load_and_sort()
+def load_and_sort():
     try:
         from sklearn.datasets import fetch_openml
         mnist = fetch_openml('mnist_784', version=1, cache=True)
@@ -42,7 +43,7 @@ def load_and_sort()
     except ImportError:
         from sklearn.datasets import fetch_mldata
         mnist = fetch_mldata('MNIST original')
-    mnist["data"], mnist["target"]
+    return mnist["data"], mnist["target"]
 
 
 def sort_by_target(mnist):
@@ -54,19 +55,64 @@ def sort_by_target(mnist):
     mnist.target[60000:] = mnist.target[reorder_test + 60000]
 
 
-def train_predict(some_digit):
+def train_predict(some_digit, X, y):
     import numpy as np
     shuffle_index = np.random.permutation(60000)
-    X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+    X_train, y_train = X[shuffle_index], y[shuffle_index]
+    X_test, y_test = X[60000:], y[60000:]
 
     # Example: Binary number 4 Classifier
-    y_train_4 = (y_train == 4)
-    y_test_4 = (y_test == 4)
+    #y_train_4 = (y_train == 4)
+    #y_test_4 = (y_test == 4)
+
+    y_train_5 = (y_train == 5)
+    y_test_5 = (y_test == 5)
+
 
     from sklearn.linear_model import SGDClassifier
     # TODO
     # print prediction result of the given input some_digit
-    
-    
-def calculate_cross_val_score():
+    sgd = SGDClassifier(random_state = 42)
+    sgd.fit(X_train, y_train_5)
+    prediction = sgd.predict(X_test)
+    n_correct = sum(prediction == y_test_5)
+    print("accuracy of test data predictions: ", n_correct / len(prediction))
+    return sgd, prediction
+
+def calculate_cross_val_score(sgd, X, y):
     # TODO
+    from sklearn.model_selection import cross_val_score
+    scores = cross_val_score(sgd, X, y, cv=3, scoring='accuracy')
+    return scores
+
+
+if __name__ == "__main__":
+    from sklearn.linear_model import SGDClassifier
+    print('Loading data from sklearn.dataset ...')
+    X, y = load_and_sort()
+    print('Train SGDClassifier moedel with training dataset ...')
+    sgd, prediction = train_predict(5, X, y)
+    print('Model training completed')
+    
+    #calculate_cross_val_score
+    sgd1 = SGDClassifier(random_state = 42)
+    scores = calculate_cross_val_score(sgd1, X, y)
+    print('cross-val scores: ', scores)
+
+    # test out a prediction from the trained SGD model
+    from random import seed
+    from random import randint
+    seed(10)
+    print('Test out a prediction from the trained SGD model ...')
+    ind = randint(0, 70000)
+    pred = sgd.predict(X[ind].reshape(1, -1))
+    print('single digit prediction: ', pred, ', actual digit: ', y[ind])
+    random_digit(X, ind)
+    print('Test out a prediction from the trained SGD model ...')
+    ind = randint(0, 70000)
+    pred = sgd.predict(X[ind].reshape(1, -1))
+    print('single digit prediction: ', pred, ', actual digit: ', y[ind])
+    random_digit(X, ind)
+
+
+

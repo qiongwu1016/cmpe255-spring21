50c6a9d403e666a1c03281e9d6329a73836e3abd
qiongwu1016 <qiongwu1016@gmail.com>
2021-03-19 23:42:20 -0700

    lab3 1st. update

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -3,34 +3,39 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
 
         
 class DiabetesClassifier:
     def __init__(self) -> None:
         col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
         self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
-        print(self.pima.head())
+        #print(self.pima.head())
         self.X_test = None
         self.y_test = None
         
 
-    def define_feature(self):
-        feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
-        X = self.pima[feature_cols]
+    def define_feature(self, cols):
+        #print('Feature selected: ', cols)
+    
+        X = self.pima[cols]
         y = self.pima.label
         return X, y
     
-    def train(self):
+    def train(self, cols):
         # split X and y into training and testing sets
-        X, y = self.define_feature()
+        X, y = self.define_feature(cols)
+        scaler = MinMaxScaler()
+        scaler.fit(X)
+        #X = scaler.transform(X)
         X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
         # train a logistic regression model on the training set
         logreg = LogisticRegression()
         logreg.fit(X_train, y_train)
         return logreg
     
-    def predict(self):
-        model = self.train()
+    def predict(self, cols):
+        model = self.train(cols)
         y_pred_class = model.predict(self.X_test)
         return y_pred_class
 
@@ -50,11 +55,26 @@ class DiabetesClassifier:
         return metrics.confusion_matrix(self.y_test, result)
     
 if __name__ == "__main__":
-    classifer = DiabetesClassifier()
-    result = classifer.predict()
-    print(f"Predicition={result}")
-    score = classifer.calculate_accuracy(result)
-    print(f"score={score}")
-    con_matrix = classifer.confusion_matrix(result)
-    print(f"confusion_matrix=${con_matrix}")
+    cols = [['pregnant', 'insulin', 'bmi', 'age'],['pregnant', 'bmi', 'pedigree', 'skin'], ['insulin','glucose', 'pregnant','pedigree']]
+
+    #classifer = DiabetesClassifier()
+    #result = classifer.predict(cols[2])
+    #print(f"Predicition={result}")
+    #score = classifer.calculate_accuracy(result)
+    #print(f"score={score}")
+    #con_matrix = classifer.confusion_matrix(result)
+    #print(f"confusion_matrix=${con_matrix}")
+ 
     
+
+    print('| Experiement | Accuracy | Confusion Matrix | Comment |')
+    print('|-------------|----------|------------------|---------|')
+    print('| Baseline    | 0.6770833333333334 | [[114  16] [ 46  16]] |  |')
+    for i in range(3):
+        classifer = DiabetesClassifier()
+        result = classifer.predict(cols[i])
+        score = classifer.calculate_accuracy(result)
+        con_matrix = classifer.confusion_matrix(result)
+        print(f'| Solution %d| {score} | {con_matrix.tolist()} | {cols[i]} |'%(i+1))
+
+

c025b68f733dfa539ca82d981675e217d9e19065
qiongwu1016 <qiongwu1016@gmail.com>
2021-03-22 13:38:36 -0700

    add bucketize feature transfrom

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.preprocessing import KBinsDiscretizer
 #from sklearn.preprocessing import MinMaxScaler
 
         
@@ -55,6 +56,16 @@ class DiabetesClassifier:
     
     def confusion_matrix(self, result):
         return metrics.confusion_matrix(self.y_test, result)
+
+    def bucketize_a_feature(self, col):
+        X = self.pima[col]
+        est = KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='uniform')
+        est.fit(X.values.reshape(-1, 1))
+        bc_X = est.transform(X.values.reshape(-1,1))
+        self.pima[col] = bc_X
+       
+
+
     
 if __name__ == "__main__":
     #classifer = DiabetesClassifier()
@@ -81,21 +92,45 @@ if __name__ == "__main__":
     print('|-------------|----------|------------------|---------|')
     print('| Baseline    | 0.6770833333333334 | [[114  16] [ 46  16]] |  |')
 
+    #solution 1-3
     for i in range(3):
-        classifer = DiabetesClassifier()
-        result = classifer.predict(cols[i])
-        score = classifer.calculate_accuracy(result)
-        con_matrix = classifer.confusion_matrix(result)
+        classifier = DiabetesClassifier()
+        result = classifier.predict(cols[i])
+        score = classifier.calculate_accuracy(result)
+        con_matrix = classifier.confusion_matrix(result)
         print(f'| Solution %d| {score} | {con_matrix.tolist()} | {cols[i]} |'%(i+1))
 
-    print('Solution 3 has the highest prediction accuracy on test dataset. ')
+    classifier_best = classifier
+
+    
+    #solution 4
+    features = ['pregnant', 'bmi', 'glucose', 'bp']
+    classifier = DiabetesClassifier()
+    classifier.bucketize_a_feature('bp')
+    result = classifier.predict(features)
+    score = classifier.calculate_accuracy(result)
+    con_matrix = classifier.confusion_matrix(result)
+    print(f'| Solution 4| {score} | {con_matrix.tolist()} | {features} bucketized bp |')
+
+    #solution 5
+    features = ['pregnant', 'bmi', 'glucose', 'bp']
+    classifier = DiabetesClassifier()
+    classifier.bucketize_a_feature('bmi')
+    result = classifier.predict(features)
+    score = classifier.calculate_accuracy(result)
+    con_matrix = classifier.confusion_matrix(result)
+    print(f'| Solution 5| {score} | {con_matrix.tolist()} | {features} bucketized bmi |')
+
+
+    print('\nSolution 3 has the highest prediction accuracy on test dataset. ')
+    result = classifier_best.predict(cols[i])
+    score = classifier_best.calculate_accuracy(result)
+    con_matrix = classifier_best.confusion_matrix(result)
     print('TP = ', con_matrix[0, 0])
     print('TN = ', con_matrix[1, 1])
     print('FP = ', con_matrix[0, 1])
     print('FN = ', con_matrix[1, 0])
     print('Recall =', con_matrix[0, 0] / (con_matrix[0, 0] + con_matrix[1, 0]))
     print('Precision = ', con_matrix[0,0] / (con_matrix[0,0] + con_matrix[0, 1]))
-
-
 
 
